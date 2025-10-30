e5a82301cd2536f7fffca863fe5a4e37c6a2cd25
braulio-dev <bchamerry@gmail.com>
2025-10-30 12:11:09 -0700

    Interpreter instead of compiler

--- src/main/java/net/worldseed/particleemitter/runtime/ParticleEmitterScript.java
+++ src/main/java/net/worldseed/particleemitter/runtime/ParticleEmitterScript.java
@@ -1,21 +1,33 @@
 package net.worldseed.particleemitter.runtime;
 
-import net.hollowcube.mql.jit.MqlCompiler;
+import net.hollowcube.mql.MqlScript;
+import net.hollowcube.mql.runtime.MqlMath;
+import net.hollowcube.mql.runtime.MqlScopeImpl;
+import net.hollowcube.mql.foreign.MqlForeignFunctions;
 import net.hollowcube.mql.jit.MqlEnv;
-
-import java.lang.reflect.InvocationTargetException;
 
 public interface ParticleEmitterScript {
     double evaluate(@MqlEnv({"variable", "v"}) ParticleInterface particle);
 
-    static ParticleEmitterScript fromDouble(double value) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    static ParticleEmitterScript fromDouble(double value) {
         return fromString(Double.toString(value));
     }
 
-    static ParticleEmitterScript fromString(String s) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    static ParticleEmitterScript fromString(String s) {
         if (s == null) return fromDouble(0);
-        MqlCompiler<ParticleEmitterScript> compiler = new MqlCompiler<>(ParticleEmitterScript.class);
-        Class<ParticleEmitterScript> scriptClass = compiler.compile(s.replace("Math", "math"));
-        return scriptClass.getDeclaredConstructor().newInstance();
+
+        // Parse the script once
+        MqlScript script = MqlScript.parse(s.replace("Math", "math"));
+
+        // Return an implementation that evaluates using the interpreter
+        return particle -> {
+            // Create a scope with math functions and the particle variable
+            MqlScopeImpl.Mutable scope = new MqlScopeImpl.Mutable();
+            scope.set("math", MqlMath.INSTANCE);
+            scope.set("variable", MqlForeignFunctions.create(ParticleInterface.class, particle));
+            scope.set("v", MqlForeignFunctions.create(ParticleInterface.class, particle));
+
+            return script.evaluate(scope);
+        };
     }
 }
